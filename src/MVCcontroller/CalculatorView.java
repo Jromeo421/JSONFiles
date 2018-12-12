d9c6a2e77a949c066f64cc08b81fba6174203a80
JJ Hugh <hug17010@byui.edu>
2018-12-11 21:47:35 -0600

    Updates

--- src/MVCcontroller/CalculatorView.java
+++ src/MVCcontroller/CalculatorView.java
@@ -20,7 +20,7 @@ public class CalculatorView extends JFrame{
         JPanel calcPanel = new JPanel();
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 200);
+        this.setSize(500, 200);
                 
         calcPanel.add(firstInteger);
         calcPanel.add(additionLabel);
