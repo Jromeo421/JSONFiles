698697ba2e9b88c88107bd3850b9126ab96bc3b5
JJ Hugh <hug17010@byui.edu>
2018-12-17 10:12:47 -0600

    Updated Portfolia FIles

--- src/JUnit/JUnit.java
+++ src/JUnit/JUnit.java
@@ -18,9 +18,9 @@ public class JUnit {
         public static void main(String[] args) {
          Result endGame = JUnitCore.runClasses(TestAssertions.class);
 
-         endGame.getFailures().forEach((failure) -> {
-             System.out.println(failure.toString());
-            });
+         for(Failure failure : endGame.getFailures()) {
+            System.out.println(failure.toString());
+         }
          
          System.out.println(endGame.wasSuccessful());
          
